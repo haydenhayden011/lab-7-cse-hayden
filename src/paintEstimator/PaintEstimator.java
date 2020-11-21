a3114f9811a21c34609f40beb60aed561740f1ad
BuildTools <unconfigured@null.spigotmc.org>
2020-11-20 20:30:16 -0500

    lab 10 update

--- src/paintEstimator/PaintEstimator.java
+++ src/paintEstimator/PaintEstimator.java
@@ -1,10 +1,24 @@
 package paintEstimator;
 
+import java.util.Scanner;
+import java.lang.Math;     // Note: Needed for math functions in part (3)
+
 public class PaintEstimator {
+   public static void main(String[] args) {
+       Scanner scnr=new Scanner(System.in);
+       double wallHeight;
+       double wallWidth;
+       double wallArea;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+       System.out.println("Enter wall height (feet):");
+       wallHeight=scnr.nextDouble();
+       System.out.println("Enter wall width (feet):");
+       wallWidth=scnr.nextDouble();
+       wallArea=wallWidth*wallHeight;
+       System.out.println("Wall area: "+wallArea+" square feet");
 
-	}
-
+       double gallons=wallArea/350.0;
+       System.out.println("Paint needed: "+gallons+" gallons");
+       System.out.println("Cans needed: "+Math.round(gallons)+" can(s)");
+   }
 }
